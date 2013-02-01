c2e012e0d42e19208055edae59f11e62118517ed
spline <reticulatingspline@github.com>
2013-02-01 16:18:29 -0500

    Delete metals

--- metals.py
+++
@@ -1,7 +0,0 @@
-# http://www.xmlcharts.com/cache/precious-metals.xml
-# http://quotes.instaforex.com/get_quotes.php?m=json
-# http://drayah.no.de/metals/latest
-# http://www.xmlcharts.com/cache/precious-metals.xml
-02:05 < quoth> Crude: 83.28 (-3.25 -3.76%) Gold: 1625.65 (+61.45 +3.93%) Silver: 28.475 (+0.72 +2.59%) Platinum: 1444.05 (+26.45 +1.87%) Palladium: 613.80 (-0.10 -0.02%)
-# http://money.cnn.com/data/commodities/
-
