5f50f635da600342a79468a1e5f72c62c65afb18
spline <reticulatingspline@github.com>
2014-10-11 14:08:51 -0400

    Add in the rest of the tests.

--- test.py
+++ test.py
@@ -13,7 +13,16 @@ class OddsTestCase(PluginTestCase):
 def testStock(self):
         self.assertRegexp('bonds', 'Treasury')
         self.assertRegexp('currency USDCAD', 'USD to CAD')
-
+        self.assertRegexp('gold', 'Gold')
+        self.assertRegexp('googlequote GOOG', 'Google')
+        self.assertRegexp('grains', 'Corn')
+        self.assertRegexp('indices', 'Dow Jones')
+        self.assertRegexp('intlindicies', 'HANG SENG')
+        self.assertRegexp('metals', 'Gold')
+        self.assertRegexp('oil', 'Crude Oil')
+        self.assertRegexp('stock GOOG', 'Google')
+        self.assertRegexp('symbolsearch Google', 'Google')
+        self.assertRegexp('yahooquote GOOG', 'Google')
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
 
