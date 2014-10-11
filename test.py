d0a7575c29dad5d8537e22581c842fafee256cd9
spline <reticulatingspline@github.com>
2014-10-11 14:15:03 -0400

    Fix tests.

--- test.py
+++ test.py
@@ -11,18 +11,18 @@ class OddsTestCase(PluginTestCase):
     plugins = ('Stock',)
 
 def testStock(self):
-        self.assertRegexp('bonds', 'Treasury')
-        self.assertRegexp('currency USDCAD', 'USD to CAD')
-        self.assertRegexp('gold', 'Gold')
-        self.assertRegexp('googlequote GOOG', 'Google')
-        self.assertRegexp('grains', 'Corn')
-        self.assertRegexp('indices', 'Dow Jones')
-        self.assertRegexp('intlindicies', 'HANG SENG')
-        self.assertRegexp('metals', 'Gold')
-        self.assertRegexp('oil', 'Crude Oil')
-        self.assertRegexp('stock GOOG', 'Google')
-        self.assertRegexp('symbolsearch Google', 'Google')
-        self.assertRegexp('yahooquote GOOG', 'Google')
+    self.assertRegexp('bonds', 'Treasury')
+    self.assertRegexp('currency USDCAD', 'USD to CAD')
+    self.assertRegexp('gold', 'Gold')
+    self.assertRegexp('googlequote GOOG', 'Google')
+    self.assertRegexp('grains', 'Corn')
+    self.assertRegexp('indices', 'Dow Jones')
+    self.assertRegexp('intlindicies', 'HANG SENG')
+    self.assertRegexp('metals', 'Gold')
+    self.assertRegexp('oil', 'Crude Oil')
+    self.assertRegexp('stock GOOG', 'Google')
+    self.assertRegexp('symbolsearch Google', 'Google')
+    self.assertRegexp('yahooquote GOOG', 'Google')
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
 
